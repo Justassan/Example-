ef59807b58f89bdde7e1ff33d39a63f4f2fdfb42
Asan <pn85414@gmail.com>
2022-11-23 23:04:36 +0600

    add new Welcom console

---
+++ Example_005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя!");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "king")
+{
+Console.WriteLine("ДАРОВА, King");
+}
+else 
+{
+    Console.Write("q, ");
+    Console.WriteLine(username);
+}
